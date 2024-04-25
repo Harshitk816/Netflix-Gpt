52cf51e07bd9af6ba6a99a283f39543daf2fdffc
Harshitk816 <harshitkaushal816@gmail.com>
2024-04-25 14:43:18 +0530

    movie detail in gpt search done

--- src/Components/MovieDetail.js
+++ src/Components/MovieDetail.js
@@ -17,14 +17,14 @@ const MovieDetail = () => {
     
     <div className='w-full h-full pt-[5%] md:pt-[5%] lg:pt-[5%] px-6 md:px-24 lg:px-24 text-white bg-gradient-to-r from-black'>
     <img 
-     className='w-36'
+     className='w-24 md:w-36 lg:w-36'
         alt='Movie Card'
         src={IMG_CDN_URL+movieDetail.posterPath}>
           
         </img>
     
         <h1 className='text-xl md:text-6xl lg:text-6xl font-bold drop-shadow'>{movieDetail.title}</h1>
-        <p className=' py-6 text-lg w-full drop-shadow'>{movieDetail.description}</p>
+        <p className=' py-6 text-lg w-full drop-shadow overflow-hidden'>{movieDetail.description}</p>
         <div className=' flex gap-3 pt-3 md:pt-0 lg:pt-0'>
             <button className='bg-white text-black font-bold px-4 md:px-8 lg:px-8 p-1 md:p-2 lg:p-2 text-sm md:text-lg lg:text-lg rounded-md hover:bg-opacity-50'><i className='bx bx-play bx-xs'></i>Play</button>
             <button className='mx-2 bg-gray-500 bg-opacity-50 text-white font-bold px-4 md:px-8 lg:px-8 p-1 md:p-2 lg:p-2 text-sm md:text-lg lg:text-lg rounded-md'><i className='bx bx-info-circle' ></i> More Info</button>
