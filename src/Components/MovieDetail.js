0918d4c7e26dfe35ec0e0927b99cd159af7e2431
Harshitk816 <harshitkaushal816@gmail.com>
2024-04-25 14:33:56 +0530

    movie detail in gpt search done

--- src/Components/MovieDetail.js
+++ src/Components/MovieDetail.js
@@ -24,8 +24,8 @@ const MovieDetail = () => {
         </img>
     
         <h1 className='text-xl md:text-6xl lg:text-6xl font-bold drop-shadow'>{movieDetail.title}</h1>
-        <p className=' py-6 text-xl w-full drop-shadow'>{movieDetail.description}</p>
-        <div className=' flex flex-wrap gap-3 pt-3 md:pt-0 lg:pt-0'>
+        <p className=' py-6 text-lg w-full drop-shadow'>{movieDetail.description}</p>
+        <div className=' flex gap-3 pt-3 md:pt-0 lg:pt-0'>
             <button className='bg-white text-black font-bold px-4 md:px-8 lg:px-8 p-1 md:p-2 lg:p-2 text-sm md:text-lg lg:text-lg rounded-md hover:bg-opacity-50'><i className='bx bx-play bx-xs'></i>Play</button>
             <button className='mx-2 bg-gray-500 bg-opacity-50 text-white font-bold px-4 md:px-8 lg:px-8 p-1 md:p-2 lg:p-2 text-sm md:text-lg lg:text-lg rounded-md'><i className='bx bx-info-circle' ></i> More Info</button>
             <button className='bg-red-700 text-white font-bold px-4 md:px-8 lg:px-8 p-1 md:p-2 lg:p-2 text-sm md:text-lg lg:text-lg rounded-md' onClick={handleMovieDetailView}>Close</button>
